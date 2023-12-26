de4ce7ee9239153962843d737461d4b7561f40db
Van Phung <vanphung030792@gmail.com>
2023-12-26 16:30:07 -0500

    done

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
    *  Loop over ranks
    *    Out the HTML for a card
    */
+  
   $suits = ['clubs', 'diamonds', 'hearts', 'spades'];
   $ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'];
 
